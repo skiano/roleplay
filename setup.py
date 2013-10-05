913d98914408179f9b89f3bbf7d44382a3bac11f
Greg Skiano <gregoryskiano@gmail.com>
2013-10-05 02:38:49 -0400

    adding director to setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name='RolePlay',
   version='0.1',
-  packages=['roleplay','roleplay.scanner'],
+  packages=['roleplay','roleplay.scanner','roleplay.director'],
   license='MIT',
   install_requires=[
     "PyPNG >= 0.0.15"
